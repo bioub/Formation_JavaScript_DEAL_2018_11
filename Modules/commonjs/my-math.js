b95141c0c679e5da24d33c6dd3525caa3f54b6f8
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2018-11-27 13:58:06 +0100

    Modules

---
+++ Modules/commonjs/my-math.js
@@ -0,0 +1,14 @@
+// (function (exports, require, module, __filename, __dirname) {
+/**
+ * Additionne 2 paramètres
+ * @param {number} a Le 1er param
+ * @param {number} b Le 2e param
+ * @returns {number} La somme
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
+
+// });
---
+++ Modules/commonjs/test-my-math.js
@@ -0,0 +1,4 @@
+// (function (exports, require, module, __filename, __dirname) {
+const add = require('./my-math');
+console.log(add(1, 2));
+// });
