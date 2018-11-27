319b6872281a40baa58719352b19344ea8443b43
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2018-11-27 13:41:10 +0100

    Correction ES6

--- Langage/ex-jeu-es6.js
+++ Langage/ex-jeu-es6.js
@@ -1,22 +1,22 @@
 
 const random = {
   // 1 - method properties
-  get: function () {
+  get() {
     return Math.random();
   },
 
   // 2 - default params
-  getArbitrary: function (min, max) {
+  getArbitrary(min = 0, max = 100) {
     return Math.random() * (max - min) + min;
   },
 
-  getInt: function (min, max) {
+  getInt(min = 0, max = 100) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   },
 
-  getIntInclusive: function (min, max) {
+  getIntInclusive(min = 0, max = 100) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min +1)) + min;
@@ -27,49 +27,46 @@ const random = {
 const readline = require('readline');
 
 // 3 - class
-function Jeu(options /* 4 - default param */) {
-  options = options || {};
+class Jeu {
+  constructor(options = {}) {
+    //options = options || {};
 
-  // 5 - destructurer options (en une ligne) avec default param (comme z page 130)
-  const min = options.min || 0;
-  const max = options.max !== undefined ? options.max : 100;
+    // 5 - destructurer options (en une ligne) avec default param (comme z page 130)
+    // const min = options.min || 0;
+    // const max = options.max !== undefined ? options.max : 100;
+    const { min = 0, max = 100 } = options;
 
-  this.rl = readline.createInterface(process.stdin, process.stdout);
-  this.entierAlea = random.getIntInclusive(min, max);
-  this.essais = [];
+
+    this.rl = readline.createInterface(process.stdin, process.stdout);
+    this.entierAlea = random.getIntInclusive(min, max);
+    this.essais = [];
+  }
+  jouer() {
+    if (this.essais.length) {
+      // 6 - template literal/string
+      console.log(`Vous avez déjà joué ${this.essais.join(' - ')}`);
+    }
+    this.rl.question('Quel est le nombre à deviner ? ', (reponse) => {
+      const entierSaisi = Number.parseInt(reponse);
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Erreur: il faut saisir un nombre');
+        return this.jouer();
+      }
+      this.essais.push(entierSaisi);
+      if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand');
+        return this.jouer();
+      }
+      if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit');
+        return this.jouer();
+      }
+      console.log('Gagné');
+      this.rl.close();
+    });
+  }
 }
 
-Jeu.prototype.jouer = function() {
-  if (this.essais.length) {
-    // 6 - template literal/string
-    console.log('Vous avez déjà joué : ' + this.essais.join(' - '));
-  }
-
-  this.rl.question('Quel est le nombre à deviner ? ', (reponse) => {
-
-    const entierSaisi = parseInt(reponse);
-
-    if (isNaN(entierSaisi)) {
-      console.log('Erreur: il faut saisir un nombre');
-      return this.jouer();
-    }
-
-    this.essais.push(entierSaisi);
-
-    if (entierSaisi > this.entierAlea) {
-      console.log('Trop grand');
-      return this.jouer();
-    }
-
-    if (entierSaisi < this.entierAlea) {
-      console.log('Trop petit');
-      return this.jouer();
-    }
-
-    console.log('Gagné');
-    this.rl.close();
-  });
-};
 
 const game = new Jeu();
 game.jouer();
