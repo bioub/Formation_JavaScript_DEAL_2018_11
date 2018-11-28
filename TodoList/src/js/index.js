d21b79a03770b95523a48f2869eb4e1825e7cc70
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2018-11-28 16:21:47 +0100

    Exercice DOM

--- TodoList/src/js/index.js
+++ TodoList/src/js/index.js
@@ -14,3 +14,23 @@ todoFormElt.addEventListener('submit', (event) => {
 
   addTodo(todo, todoContainerElt);
 });
+
+
+/*
+Exercice :
+
+1 / Dans addTodo, ajouter un bouton moins
+Au clic de ce bouton moins, supprimer la balise div
+parent
+Indice MDN : parentNode, removeChild()
+
+2 / Ajouter un case à cocher dans index.html
+Ajouter une case à cocher dans addTodo
+Au clic de la case à cocher de index.html
+Cocher ou décocher toutes les autres
+
+Indice MDN : querySelectorAll, Array.from
+checkboxElt.checked = true pour cocher
+checkboxElt.checked = false pour décocher
+
+*/
